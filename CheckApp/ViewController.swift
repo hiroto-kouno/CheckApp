e1b8942af9549b16e48986d6476e71b67a71a5f6
hiroto.kouno <h-kono@i-enter.co.jp>
2022-06-03 10:07:21 +0900

    Initial Commit

---
+++ CheckApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckApp
+//
+//  Created by 河野 裕門 on 2022/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
